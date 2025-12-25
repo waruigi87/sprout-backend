a1f0204a2fad05abbe6f3c01f1bfdc5decd94ce8
Akifumi Nakayama <akifuminakayama@AkifuminoMacBook-Air.local>
2025-12-25 16:25:07 +0900

    TodoItem.php編集

--- app/Models/ToDoItem.php
+++ app/Models/ToDoItem.php
@@ -9,11 +9,25 @@ class ToDoItem extends Model
 {
     use HasFactory;
 
-    // ▼ これを追加して、使うテーブル名を明示します
+    /**
+     * テーブル名を指定
+     * (指定しないとLaravelは 'to_do_items' を探しにいってしまうため必須)
+     */
     protected $table = 'todo_items';
 
-    protected $fillable = ['class_id', 'content', 'is_completed'];
-    
+    /**
+     * 一括割り当て（createやupdate）を許可するカラム
+     */
+    protected $fillable = [
+        'class_id',
+        'content',
+        'is_completed',
+    ];
+
+    /**
+     * ネイティブな型へのキャスト
+     * (DB上の 0/1 を PHP/JSON上の false/true に変換)
+     */
     protected $casts = [
         'is_completed' => 'boolean',
     ];
